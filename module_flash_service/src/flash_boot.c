50652dc63a303fec6b4eec2ab161456e62eb41aa
Henrik Stroetgen <hstroetgen@synapticon.com>
2015-12-18 18:56:16 +0100

    add flash_boot.c and flash_data.c

---
+++ module_flash_service/src/flash_boot.c
@@ -0,0 +1,18 @@
+/*
+ * flash_boot.xc
+ *
+ *  Created on: 17.12.2015
+ *      Author: hstroetgen
+ */
+
+#include <flash_boot.h>
+
+int flash_write_boot(char data[], unsigned size)
+{
+
+}
+
+int flash_read_boot(char data[], unsigned size)
+{
+
+}

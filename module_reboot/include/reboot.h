e9acfa222ceee5fd4587aecc33f01d68ba3681b0
Simon Fischinger <mail@simon-fischinger.de>
2017-04-12 20:21:28 +0200

    module_reboot: Initial documentation commit

--- module_reboot/include/reboot.h
+++ module_reboot/include/reboot.h
@@ -18,17 +18,67 @@
 
 #pragma once
 
-typedef interface RebootInterface {
+/**
+ * @brief Interface to communicate with the position service
+ */
+typedef interface RebootInterface 
+{
+    /**
+    * @brief Reboot all tiles of the device
+    */
     void device_reboot(void);
+
+    /**
+    * @brief INTERNAL USE ONLY
+    * Returns the value of the bootflag set
+    * 
+    * @return Value of bootflag
+    */
     unsigned get_boot_flag(void);
+
+    /**
+    * @brief INTERNAL USE ONLY
+    * Sets a value for the bootflag
+    *
+    * @param Bootflag value
+    */
     void set_boot_flag(unsigned);
+
+    /**
+    * @brief INTERNAL USE ONLY
+    * Boots from the application to the bootloader
+    */
     void boot_to_bootloader(void);
+
+    /**
+    * @brief INTERNAL USE ONLY
+    * Boots from the bootloader to the application
+    */
     void boot_to_application(void);
+
+    /**
+    * @brief INTERNAL USE ONLY
+    * Checks if the was a cold-boot or if the application requested a reboot to the bootloader.
+    *
+    * @return 1 if reboot to bootloader was requested by application
+    */
     int has_rebooted_from_application(void);
+
 } RebootInterface;
 
-
+/**
+* @brief Service to accept reboot commands from clients.
+* 
+* @param i_reboot Interface between reboot service and user task (client)
+*/
 void reboot_service(server interface RebootInterface i_reboot);
+
+/**
+* @brief INTERNAL USE ONLY
+*
+* Function to execute a SoC reset
+* 
+*/
 void reboot_device(void);
 
 #ifdef COM_ETHERCAT

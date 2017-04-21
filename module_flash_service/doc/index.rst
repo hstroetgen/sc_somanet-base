a966dc0c35025b133ccb4ced4bb522774a456efc
Simon Fischinger <mail@simon-fischinger.de>
2017-04-21 23:01:12 +0200

    Documentation: Start of documentation of module_flash_service

--- module_flash_service/doc/index.rst
+++ module_flash_service/doc/index.rst
@@ -1,11 +1,43 @@
+.. _module_flash_service:
+
 =====================
 SOMANET Flash Service
 =====================
+.. contents:: In this document
+    :backlinks: none
+    :depth: 3
 
-This module offers...
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque ut ante a metus facilisis dictum. Sed ac erat a quam porta posuere at eu est. Phasellus congue suscipit semper. Pellentesque mollis tristique lorem, ac hendrerit ligula egestas elementum. Donec vestibulum eros ex, quis volutpat mauris tempor a. Pellentesque quis purus non ligula gravida vulputate. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;
 
 .. cssclass:: github
 
   `See Module on Public Repository <https://github.com/synapticon/sc_somanet-base/tree/master/module_flash_service>`_
-  
-.. important:: This module is still not completed. Currently it is not required by any SOMANET Library and its use is not recommended. 
+
+|
+|
+
+.. note:: The usage of this module can erase all content of flash and render your SoC unusable. 
+
+.. important:: The usage of this module can erase all content of flash and render your SoC unusable.
+
+
+
+How to use
+==========
+
+
+
+
+API
+===
+
+Service
+--------
+
+.. doxygenfunction:: flash_service
+
+Interface
+---------
+
+.. doxygeninterface:: FlashBootInterface
+
--- module_flash_service/include/flash_service.h
+++ module_flash_service/include/flash_service.h
@@ -14,16 +14,50 @@
 #endif
 
 interface FlashDataInterface {
+
     [[guarded]] int get_configurations(int type, unsigned char buffer[], unsigned &n_bytes);
     [[guarded]] int set_configurations(int type, unsigned char data[n_bytes], unsigned n_bytes);
 };
 typedef interface FlashDataInterface FlashDataInterface;
 
-interface FlashBootInterface {
+
+/**
+ * @brief Interface to access boot partition of SOMANET SoCs
+ */
+interface FlashBootInterface 
+{
+    /**
+     * @brief NOT YET IMPLEMENTED
+     */
     [[guarded]] int read(char data[], unsigned nbytes,  unsigned char image_num);
+    
+    /**
+     * @brief Writes chunk of data to boot partition of flash
+     * @param data[] Char array holding data to be written to flash
+     * @param nbytes Number of bytes to be written
+     * @return 0 - success 
+     */
     [[guarded]] int write(char data[], unsigned nbytes);
+
+    /**
+     * @brief Prepares the boot partition for a new boot image to be written
+     * @return 0 - success
+     */
     [[guarded]] int prepare_boot_partition();
+
+    /**
+     * @brief Deletes the boot partition of the SoC. 
+     * Note: This will remove the running program from flash, after this function is called, the device 
+     * will not boot after a power cycle. 
+     * 
+     * @return 0 - success
+     */
     [[guarded]] void erase_boot_partition(void);
+
+    /**
+     * @brief Checks if flash contains a valid factory and upgrade image
+     * @return 0 - if valid images exist
+     */
     [[guarded]] int validate_flashing(void);
     [[guarded]] int upgrade_image_installed(void);
 
@@ -40,6 +74,15 @@ enum configuration_type {
     MOTCTRL_CONFIG
 };
 
+
+/**
+ * @brief Service to access the data partition of the internal flash of Synapticon SoCs
+ *
+ * @param SPI  SPI port definition struct 
+ * @param i_boot Interface for boot partition manipulation    
+ * @param i_data Interface for data partition manipulation
+ * @param n_data Number of end-points in i_data array
+ */
 #ifdef XCORE200
 void flash_service(fl_QSPIPorts &SPI,
                    interface FlashBootInterface server ?i_boot,
